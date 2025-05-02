836bf411fe91562094053d8f4dd5b20c9d976447
Bernard Chan <chanlhock@gmail.com>
2025-05-02 12:44:48 +0800

    Clean up

--- app/src/main/java/com/example/slapimage/newcalculator/screens/DataScreen.kt
+++ app/src/main/java/com/example/slapimage/newcalculator/screens/DataScreen.kt
@@ -2,10 +2,6 @@ package com.example.slapimage.newcalculator.screens
 
 import com.example.slapimage.newcalculator.CalculationAdapter
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
