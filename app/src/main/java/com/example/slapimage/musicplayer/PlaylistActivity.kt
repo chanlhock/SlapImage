ba9d9141aaff9c85734f5511f6a18df45c60805a
Bernard Chan <chanlhock@gmail.com>
2025-05-07 10:29:54 +0800

    Tidied up

--- app/src/main/java/com/example/slapimage/musicplayer/PlaylistActivity.kt
+++ app/src/main/java/com/example/slapimage/musicplayer/PlaylistActivity.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.example.slapimage.databinding.ActivityPlaylistBinding
